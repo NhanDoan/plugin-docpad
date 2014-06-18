f80f3fff76c5c156134d9905ebca0f8ab2085b05
Dung <dung.hoduy@asnet.com.vn>
2014-06-18 08:17:38 +0700

    Check status code in return results

--- www/app/helpers/Helpers.php
+++ www/app/helpers/Helpers.php
@@ -1,88 +1,88 @@
 <?php
 class Helpers {
 
-		/**
-		 * execute curl
-     * @param  string $uri    url of api
-     * @param  array  $params get paramater
-     * @return string  resutl with json format
-		 */
+    /**
+    * execute curl
+    * @param  string $uri    url of api
+    * @param  array  $params get paramater
+    * @return string  resutl with json format
+    */
     public static function curl_execute($uri, $params = array()) {
-    	
-    	$ch = curl_init();
-    	
-	    //set the url, number of POST vars, POST data
-	    curl_setopt($ch, CURLOPT_URL, $uri);
-	    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-    	curl_setopt($ch, CURLOPT_FORBID_REUSE, 0);
 
-	    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	    curl_setopt($ch, CURLOPT_HEADER, 0);
-	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	    // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,1);
-	    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        $ch = curl_init();
 
-			if ( !empty($params) ) {
+        //set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_URL, $uri);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_FORBID_REUSE, 0);
 
-				$queryString = Helpers::build_query_string($params);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
-				curl_setopt($ch, CURLOPT_POST, count($params));
-	    	curl_setopt($ch, CURLOPT_POSTFIELDS, $queryString);
-			}
+        if ( !empty($params) ) {
 
-	    //execute post
-	    $results = curl_exec($ch);
+            $queryString = Helpers::build_query_string($params);
 
-	    //close connection
-	    curl_close($ch);
-	    
-	    return $results;
+            curl_setopt($ch, CURLOPT_POST, count($params));
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $queryString);
+        }
+
+        //execute post
+        $results = curl_exec($ch);
+
+        //close connection
+        curl_close($ch);
+
+        return $results;
 
     }
 
     /**
-     * json decode result when call api
-     * @param $str string json result
-     * @return array list lender
-     */
+    * json decode result when call api
+    * @param $str string json result
+    * @return array list lender
+    */
     public static function format_result( $str ) {
-    	$results = json_decode( $str, true );
+        $results = json_decode( $str, true );
 
-    	if ( !empty($results) ) {
-    		return $results['lenderOffers']['@items'];
-    	}
+        if ( !empty($results) && $results['status'] == 0 ) {
+            return $results['lenderOffers']['@items'];
+        }
 
-    	return array();
+        return array();
     }
 
     /**
-     * execute curl and format result
-     * @param  string $uri    url of api
-     * @param  array  $params get paramater
-     * @return array  results after format
-     */
+    * execute curl and format result
+    * @param  string $uri    url of api
+    * @param  array  $params get paramater
+    * @return array  results after format
+    */
     public static function curl_exec_format($uri, $params = array()) {
-    	
-    	$result_str = Helpers::curl_execute( $uri, $params );
 
-    	return Helpers::format_result($result_str);
+        $result_str = Helpers::curl_execute( $uri, $params );
+
+        return Helpers::format_result($result_str);
     }
 
     /**
-     * convert array params to querystring
-     * @param  array  $params get paramater
-     * @return string querystring
-     */
+    * convert array params to querystring
+    * @param  array  $params get paramater
+    * @return string querystring
+    */
     public static function build_query_string($params = array()) {
 
-    	foreach ( $params as $key => $value ) {
+        foreach ( $params as $key => $value ) {
 
-        $query_array[] = ((strpos($key, 'lender') !== FALSE) ? 'lender' : $key) . '=' . $value;
+            $query_array[] = ((strpos($key, 'lender') !== FALSE) ? 'lender' : $key) . '=' . $value;
 
-	    }
+        }
 
-	    return implode( '&', $query_array );
-		  
+        return implode( '&', $query_array );
+
     }
 }
